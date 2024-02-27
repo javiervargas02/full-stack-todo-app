0f9a4bae2cb77cea819161baa8423b73b6f95963
javiervargas02 <javier.v.021204@gmail.com>
2024-02-27 15:49:43 -0600

    Refactor App component to use React Router and implement user authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import NotLoggedIn from "./routes/NotLoggedIn";
+import LoggedIn from "./routes/LoggedIn";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
+import {
+  initAxiosInterceptors,
+  getUser,
+  deleteUser,
+  API_URL,
+  setUser,
+} from "./utils/helpers";
+import axios from "axios";
+import NavBar from "./components/NavBar";
 
-function App() {
+initAxiosInterceptors();
+
+export default function App() {
+  const [userInformation, setUserInformation] = useState(null);
+
+  const handleLogout = ({
+    message = "You've been logged out",
+    icon = "ℹ️",
+  }) => {
+    setUserInformation(null);
+    deleteUser();
+    toast(message, {
+      icon: icon,
+    });
+  };
+
+  useEffect(() => {
+    const user = getUser();
+    if (user && user.access_token && user.user) {
+      setUserInformation(user);
+    } else {
+      setUserInformation(null);
+      deleteUser();
+      return;
+    }
+    const loadUser = async () => {
+      setUserInformation(user);
+      try {
+        const { data } = await axios.get(`${API_URL}/auth/whoami`);
+        setUserInformation(data);
+        setUser(data);
+      } catch (err) {
+        console.error(err);
+        if (err.response && err.response.status === 401) {
+          handleLogout({ message: err.response.data.detail });
+          return;
+        }
+        toast.error("An unexpected error has occurred, please try again");
+      }
+    };
+    loadUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Toaster />
+      </div>
+      <Router>
+        {userInformation ? (
+          <>
+            <NavBar handleLogout={handleLogout} />
+            <LoggedIn
+              userInformation={userInformation}
+              setUserInformation={setUserInformation}
+            />
+          </>
+        ) : (
+          <NotLoggedIn setUserInformation={setUserInformation} />
+        )}
+      </Router>
+    </>
   );
 }
-
-export default App;
