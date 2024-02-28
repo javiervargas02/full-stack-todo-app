b961d7f5f2f876302e33845b1036169b2fe11bc3
javiervargas02 <javier.v.021204@gmail.com>
2024-02-28 08:46:59 -0600

    Update Modal component styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Modal({ children, handleClose }) {
   return (
-    <div className="absolute w-full bg-white/90 min-h-screen z-50 flex items-center">
+    <div className="absolute w-full bg-white/90 min-h-screen z-50 flex items-center px-8">
       <div className="relative border shadow-2xl rounded-xl md:w-1/2 mx-auto bg-white">
         <FontAwesomeIcon
           icon={faTimesCircle}
